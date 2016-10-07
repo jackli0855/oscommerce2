92e4ad7083e950a49fc39d0faee20f778cdd9101
libao <lb.funward@gmail.com>
2016-10-07 11:51:52 +0800

    login cn message added

---
+++ catalog/includes/languages/chinese/modules/content/login/cm_create_account_link.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CREATE_ACCOUNT_LINK_TITLE', '新建用户连接');
+  define('MODULE_CONTENT_CREATE_ACCOUNT_LINK_DESCRIPTION', '在登录页面上显示注册用户的表单');
+
+  define('MODULE_CONTENT_LOGIN_HEADING_NEW_CUSTOMER', '新用户');
+  define('MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER', '我是新用户');
+  define('MODULE_CONTENT_LOGIN_TEXT_NEW_CUSTOMER_INTRODUCTION', '在' . STORE_NAME . '创建新用户，您将获得更快的购物。'); 
+?>
---
+++ catalog/includes/languages/chinese/modules/content/login/cm_login_form.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_LOGIN_FORM_TITLE', '登录表单');
+  define('MODULE_CONTENT_LOGIN_FORM_DESCRIPTION', '在登录页面显示登录表单');
+
+  define('MODULE_CONTENT_LOGIN_HEADING_RETURNING_CUSTOMER', '老用户');
+  define('MODULE_CONTENT_LOGIN_TEXT_RETURNING_CUSTOMER', '我是老用户');
+  define('MODULE_CONTENT_LOGIN_TEXT_PASSWORD_FORGOTTEN', '忘记密码？');
+
+  define('MODULE_CONTENT_LOGIN_TEXT_LOGIN_ERROR', '错误：用户名和密码不匹配');
+?>
